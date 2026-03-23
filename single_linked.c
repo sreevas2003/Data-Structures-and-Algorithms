e4a4bb913da24123b943b0057722bd644b707578
BOYA SREENIVASULU <sreevas2003@gmail.com>
2026-03-23 22:41:23 +0530

    Update single_linked.c

--- single_linked.c
+++ single_linked.c
@@ -2,140 +2,194 @@
 #include<stdlib.h>
 struct node
 {
-	int info;
-	struct node* next;
+    int data;
+    struct node* next;
 }*head=NULL;
 typedef struct node Node;
+Node* create(int data)
+{
+    Node* temp=malloc(sizeof(Node));
+    temp->data=data;
+    temp->next=NULL;
+    return temp;
+}
 void display(Node* head)
 {
-	Node* temp=head;
-	while(temp!=NULL)
-	{
-		printf(" %d ->",temp->info);
-		temp=temp->next;
-	}
-	printf("NULL\n");
+    Node* temp=head;
+    while(temp)
+    {
+        printf("%d->",temp->data);
+        temp=temp->next;
+    }
+    printf("NULL\n");
 }
-Node* insert_begin(Node* head,int data)
+Node* insert_begin(Node* head, int data)
 {
-	Node *p;
-	p=malloc(sizeof(Node));
-	p->info=data;
-	p->next=head;
-	head=p;
-	return head;
-}
-Node* insert_end(Node* head,int data)
-{
-	Node *p,*temp;
-	p=malloc(sizeof(Node));
-	p->info=data;
-	p->next=NULL;
-	temp=head;
-	while(temp->next!=NULL)
-		temp=temp->next;
-	temp->next=p;
-	return head;
+    Node* temp=create(data);
+    temp->next=head;
+    head=temp;
+    return head;
 }
 Node* insert_pos(Node* head,int data,int pos)
 {
-	Node *p,*temp;
-	if(pos==1)
-	{
-		head=insert_begin(head,data);
-		return head;
-	}
-	p=malloc(sizeof(Node));
-	p->info=data;
-	p->next=NULL;
-	temp=head;
-	int i;
-	for(i=1;i<pos-1;i++)
-		temp=temp->next;
-	if(temp==NULL)
-	{
-		printf("Position exceeds than list length\n");
-		return head;
-	}
-	p->next=temp->next;
-	temp->next=p;
-	return head;
+    if(pos==1)
+    {
+        head=insert_begin(head,data);
+        return head;
+    }
+    Node* temp=create(data);
+    Node* p=head;
+    while(pos-2 && p)
+    {
+        p=p->next;
+        pos--;
+    }
+    temp->next=p->next;
+    p->next=temp;
+    return head;
+}
+Node* insert_end(Node* head,int data)
+{
+    Node* temp=create(data);
+    Node* p=head;
+    while(p->next)
+        p=p->next;
+    p->next=temp;
+    return head;
 }
 Node* delete_begin(Node* head)
 {
-	Node* temp=head;
-	head=temp->next;
-	free(temp);
-	return head;
+    Node* temp=head;
+    head=head->next;
+    free(temp);
+    return head;
 }
 Node* delete_end(Node* head)
 {
-	Node* temp=head;
-	while(temp->next->next!=NULL)
-		temp=temp->next;
-	Node* p=temp->next->next;
-	temp->next=NULL;
-	free(p);	
-	return head;
+    Node* temp=head;
+    while(temp->next->next)
+        temp=temp->next;
+    free(temp->next);
+    temp->next=NULL;
+    return head;
 }
 Node* delete_pos(Node* head,int pos)
 {
-	if(pos==1)
-	{
-		head=delete_begin(head);
-		return head;
-	}
-	Node* temp=head;
-	int i=1;
-	while(i<pos-1)
-	{
-		temp=temp->next;
-		i++;
-	}
-	Node* p=temp->next;
-	temp->next=p->next;
-	free(p);
-	return head;
+    Node* temp=head;
+    if(pos==1)
+    {
+        head=delete_begin(head);
+        return head;
+    }
+    while(pos-2 && temp)
+    {
+        temp=temp->next;
+        pos--;
+    }
+    Node* p=temp->next;
+    temp->next=p->next;
+    free(p);
+    return head;
+}
+void search(Node* head, int tar)
+{
+    Node* temp=head;
+    int c=0;
+    while(temp)
+    {
+        c++;
+        if(temp->data==tar)
+        {
+            printf("Element found at position %d\n",c);
+            return;
+        }
+        temp=temp->next;
+    }
+    printf("Element Not found\n");
 }
 Node* reverse(Node* head)
 {
-	Node *prev,*forw,*temp;
-	temp=head;
-	prev=NULL;
-	while(temp!=NULL)
-	{
-		forw=temp->next;
-		temp->next=prev;
-		prev=temp;
-		temp=forw;
-	}
-	head=prev;
-	return head;
+    Node* temp=head;
+    Node* prev=NULL;
+    Node* next=head;
+    while(temp)
+    {
+        next=temp->next;
+        temp->next=prev;
+        prev=temp;
+        temp=next;
+    }
+    return prev;
+}
+int middle(Node* head)
+{
+    Node *slow=head,*fast=head;
+    while(fast && fast->next)
+    {
+        slow=slow->next;
+        fast=fast->next->next;
+    }
+    return slow->data;
+}
+void loop(Node* head)
+{
+    Node *slow=head,*fast=head;
+    while(fast && fast->next)
+    {
+        slow=slow->next;
+        fast=fast->next->next;
+        if(slow==fast)
+        {
+            printf("Loop is detected\n");
+            return;
+        }
+    }
+    printf("Loop is Not detected\n");
+}
+void loop(Node* head)
+{
+    Node *slow=head,*fast=head;
+    while(fast && fast->next)
+    {
+        slow=slow->next;
+        fast=fast->next->next;
+        if(slow==fast)
+        {
+            printf("Loop is detected\n");
+            slow=head;
+            if(slow==fast)
+            {
+                while(slow != fast->next)
+                    fast=fast->next;
+            }
+            else
+            {
+                while(slow->next != fast->next)
+                {
+                    slow=slow->next;
+                    fast=fast->next;
+                }
+            }
+            fast->next=NULL;
+            return;
+        }
+    }
+    printf("Loop is Not detected\n");
 }
 void main()
 {
-	int data,n,i;
-	printf("Enter number of nodes : ");
-	scanf("%d",&n);
-	for(i=1;i<=n;i++)
-	{
-		printf("Enter data : ");
-		scanf("%d",&data);
-		if(i==1)
-		{
-			head=insert_begin(head,data);
-		}
-		else
-		{
-			head=insert_end(head,data);
-		}
-	}
-	//head=insert_begin(head,100);
-//	head=insert_end(head,200);
-	//head=insert_pos(head,500,3);
-//	head=delete_begin(head);
-	//head=delete_end(head);
-	//head=delete_pos(head,3);
-	head=reverse(head);
-	display(head);
+    head=create(10);
+    head->next=create(20);
+    head=insert_begin(head,30);
+    head=insert_end(head,40);
+    head=insert_pos(head,50,3);
+    // head=delete_begin(head);
+    // head=delete_end(head);
+    // head=delete_pos(head,3);
+    search(head,60);
+    display(head);
+    printf("middele element is %d\n",middle(head));
+    loop(head);
+    // head=reverse(head);
+    // display(head);
 }
